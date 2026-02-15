5b459832f1652b28829c7cb38d4fa8b6bd921d08
Oliver Bruns <obruns@gmail.com>
2026-02-15 19:47:48 +0100

    ch2-01: add support for 0b, 0 and 0x prefixes

--- ch2-01.sh
+++ ch2-01.sh
@@ -14,4 +14,11 @@ signed float -4.5
 just decimal point .31415
 scientific notation 1e3
 signed scientific notation -7e-10
+binary 0b01
+binary 0b010101
+oct 0755
+oct 0644
+hex 0xdeadbeef
+hex 0xdefec8ed
+hex 0xc0ffee
 EOF
