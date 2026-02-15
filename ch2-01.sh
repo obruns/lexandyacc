b2d9c214980b27142267a74be591c4d66c97ac7b
Oliver Bruns <obruns@gmail.com>
2026-02-15 19:38:49 +0100

    ch2-01: add a script to test the decimal number specification

---
+++ ch2-01.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e -o pipefail
+set -u
+set -x
+
+# echo "123 hello world 0.0 and 4.5 or .31415 is pi and e12 should also be matched and 0xdeadbeef is not" | ./ch2-01.pgm
+
+./ch2-01.pgm <<EOF
+integer 123
+negative integer -456
+float 0.0
+signed float -4.5
+just decimal point .31415
+scientific notation 1e3
+signed scientific notation -7e-10
+EOF
