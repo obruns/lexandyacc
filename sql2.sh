f88eaa6037aa1e8fa309962a32853280c2856bae
Oliver Bruns <obruns@gmail.com>
2026-02-17 18:33:42 +0100

    sql2: run the sample code for embedded SQL

---
+++ sql2.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e -o pipefail
+set -u
+
+# page 110
+./sql2 <<EOF
+char flavor[6], name[8], type[5];
+int SQLCODE;  /* global status variable */
+
+EXEC SQL DECLARE flav CURSOR FOR
+  SELECT Foods.name, Foods.type
+  FROM Foods
+  WHERE Foods.flavor = :flavor ;
+
+int main(int, char**) {
+  scanf("%s", flavor);
+  EXEC SQL OPEN flav;
+  for (;;) {
+    EXEC SQL FETCH flav INTO :name, :type;
+    if (SQLCODE != 0)
+      break;
+    printf("%8.8s %5.5s\n, name, type);
+  }
+  EXEC SQL CLOSE flav;
+}
+EOF
