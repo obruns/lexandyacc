obruns/lexandyacc

commit f7934f7db9b7e0d5ce7fef53f7b132fa2d8556b1

ch1-0{5,6}: use --file-prefix to avoid manual moves

Makefile
@@ -35,33 +35,29 @@ progs.tar:
 
 # Chapter 1
 
-ch1-05.pgm: ch1-05l.o ch1-05y.o
-	${CC} ${CFLAGS} -o ch1-05.pgm ch1-05l.o ch1-05y.o ${LIBS}
+ch1-05.pgm: ch1-05l.o ch1-05y.tab.o
+	${CC} ${CFLAGS} -o ch1-05.pgm ch1-05l.o ch1-05y.tab.o ${LIBS}
 
 ch1-05l.c:	ch1-05.l
 	${LEX} ch1-05.l
 	mv lex.yy.c ch1-05l.c
 
-ch1-05l.o:	ch1-05l.c ch1-05y.h
+ch1-05l.o:	ch1-05l.c ch1-05y.tab.h
 
-ch1-05y.h ch1-05y.c:	ch1-05.y
-	${YACC} -d ch1-05.y
-	mv y.tab.c ch1-05y.c
-	mv y.tab.h ch1-05y.h
+ch1-05y.tab.h ch1-05y.tab.c:	ch1-05.y
+	${YACC} --file-prefix=ch1-05y -d ch1-05.y
 
-ch1-06.pgm: ch1-06l.o ch1-06y.o
-	${CC} ${CFLAGS} -o ch1-06.pgm ch1-06l.o ch1-06y.o ${LIBS}
+ch1-06.pgm: ch1-06l.o ch1-06y.tab.o
+	${CC} ${CFLAGS} -o ch1-06.pgm ch1-06l.o ch1-06y.tab.o ${LIBS}
 
 ch1-06l.c:	ch1-06.l
 	${LEX} ch1-06.l
 	mv lex.yy.c ch1-06l.c
 
-ch1-06l.o:	ch1-06l.c ch1-06y.h
+ch1-06l.o:	ch1-06l.c ch1-06y.tab.h
 
-ch1-06y.h ch1-06y.c:	ch1-06.y
-	${YACC} -d ch1-06.y
-	mv y.tab.c ch1-06y.c
-	mv y.tab.h ch1-06y.h
+ch1-06y.tab.h ch1-06y.tab.c:	ch1-06.y
+	${YACC} --file-prefix=ch1-06y -d ch1-06.y
 
 # Chapter 2
 #	all use single lex source files

ch1-05.l
@@ -3,7 +3,7 @@
  * We now build a lexical analyzer to be used by a higher-level parser.
  */
 
-#include "ch1-05y.h"	/* token codes from the parser */
+#include "ch1-05y.tab.h"	/* token codes from the parser */
 
 #define LOOKUP 0 /* default - not a defined word type. */
 

ch1-06.l
@@ -3,7 +3,7 @@
  * We now build a lexical analyzer to be used by a higher-level parser.
  */
 
-#include "ch1-06y.h"	/* token codes from the parser */
+#include "ch1-06y.tab.h"	/* token codes from the parser */
 
 #define LOOKUP 0 /* default - not a defined word type. */
 

commit c7698af1c2180ea2757951e684c793f1d0b1b6ac

build: make: ch1-0{5,6}: drop explicit rules
Probably-fixes: f7934f7db9b7e0d5ce7fef53f7b132fa2d8556b1
Probably-fixes: 1b41670b0659d3083d591df08828c0b6d2c36c29

Makefile
@@ -34,28 +34,7 @@ progs.tar:
 	scn1.l scn2.l sql1.y sql2.y sqltext.c
 
 # Chapter 1
-
-ch1-05.pgm: ch1-05l.o ch1-05y.tab.o
-	${CC} ${CFLAGS} -o ch1-05.pgm ch1-05l.o ch1-05y.tab.o ${LIBS}
-
-ch1-05l.c:	ch1-05.l
-	${LEX} --outfile=$*.c ch1-05.l
-
-ch1-05l.o:	ch1-05l.c ch1-05y.tab.h
-
-ch1-05y.tab.h ch1-05y.tab.c:	ch1-05.y
-	${YACC} --file-prefix=ch1-05y -d ch1-05.y
-
-ch1-06.pgm: ch1-06l.o ch1-06y.tab.o
-	${CC} ${CFLAGS} -o ch1-06.pgm ch1-06l.o ch1-06y.tab.o ${LIBS}
-
-ch1-06l.c:	ch1-06.l
-	${LEX} --outfile=$*.c ch1-06.l
-
-ch1-06l.o:	ch1-06l.c ch1-06y.tab.h
-
-ch1-06y.tab.h ch1-06y.tab.c:	ch1-06.y
-	${YACC} --file-prefix=ch1-06y -d ch1-06.y
+#	implied by the other rules
 
 # Chapter 2
 #	all use single lex source files

ch1-05.l
@@ -3,7 +3,7 @@
  * We now build a lexical analyzer to be used by a higher-level parser.
  */
 
-#include "ch1-05y.tab.h"	/* token codes from the parser */
+#include "ch1-05.tab.h"	/* token codes from the parser */
 
 #define LOOKUP 0 /* default - not a defined word type. */
 

ch1-06.l
@@ -3,7 +3,7 @@
  * We now build a lexical analyzer to be used by a higher-level parser.
  */
 
-#include "ch1-06y.tab.h"	/* token codes from the parser */
+#include "ch1-06.tab.h"	/* token codes from the parser */
 
 #define LOOKUP 0 /* default - not a defined word type. */
 